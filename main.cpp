363b2d48bdd43a356f2429a92e4507b91d2cce28
pmankyu <pmankyu@gmail.com>
2020-10-01 14:12:28 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+    printf("Hello world!!\n");
+    return 0;
+}
