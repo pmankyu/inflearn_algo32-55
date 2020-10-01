8eb8f2c3cf27063a837c940c727c6577f8e8520e
pmankyu <pmankyu@gmail.com>
2020-10-01 14:47:12 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -8,6 +8,27 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-    printf("Hello world!!\n");
+    freopen("input.txt", "rt", stdin);
+	int a[100], n, idx, i, j, tmp;
+
+	scanf("%d", &n);
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+
+    for(i=0; i<n-1; i++){
+        for(j=0; j<n-i-1; j++){
+            if(a[j] > a[j+1]){
+                tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1]=tmp;
+            }
+        }
+    }
+
+    for(i=0; i<n; i++){
+        printf("%d ", a[i]);
+    }
+
     return 0;
 }
